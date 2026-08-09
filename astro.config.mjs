cbafdc79f92c3bbeaca43c83bf7f073e8d7633e5
dp1500 <devanshparmar10@gmail.com>
2026-08-09 20:16:58 +0530

    fix deploy 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,33 @@
+
+// import { defineConfig } from 'astro/config';
+// import react from '@astrojs/react';
+// import tailwindcss from '@tailwindcss/vite';
+
+// export default defineConfig({
+//   integrations: [react()],
+//   vite: {
+//     plugins: [tailwindcss()],
+//   },
+// });
+
+
+// @ts-check
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://dp1500.github.io',
+
+  // GitHub Pages uses /quantmorning.
+  // Local development uses /.
+  base: import.meta.env.GITHUB_ACTIONS === 'true'
+    ? '/quantmorning'
+    : '',
+
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
   },
